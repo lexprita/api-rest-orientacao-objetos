6fd0d9c7438c223124e7ec239b89c4e983ec4763
Lucas.Exprita <lucasld2@hotmail.com>
2024-08-14 20:49:28 +0100

    Implementação do listar autor

--- src/bancodedados.ts
+++ src/bancodedados.ts
@@ -1,6 +1,11 @@
 import Autor from "./modelos/Autor";
 import Post from "./modelos/Post";
 
-export const autores: Autor[] = []
+export const autores: Autor[] = [
+    new Autor({
+        nome: 'Lucas',
+        idade: 32
+    })
+]
 
 export const posts: Post[] = []
