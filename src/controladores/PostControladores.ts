50ddf92b1c3e036efbbd31190c8302b78e78cf71
Lucas.Exprita <lucasld2@hotmail.com>
2024-08-14 22:04:17 +0100

    Implementação do editar postagem

--- src/controladores/PostControladores.ts
+++ src/controladores/PostControladores.ts
@@ -57,6 +57,29 @@ export default class PostControlador {
         return res.status(201).json(post)
     }
     editar(req: Request, res: Response) {
+        const { id } = req.params
+        const { titulo, descricao } = req.body
+
+        if (!titulo || !descricao) {
+            return res.status(400).json({
+                mensagem: 'O titulo e a descrição da postagem são obrigatórios'
+            })
+        }
+
+        const post = posts.find((elemento) => {
+            return elemento.id === id
+        })
+
+        if (!post) {
+            return res.status(404).json({
+                mensagem: 'A postagem não existe'
+            })
+        }
+
+        post.titulo = titulo
+        post.descricao = descricao
+
+        return res.status(204).send()
 
     }
     excluir(req: Request, res: Response) {
